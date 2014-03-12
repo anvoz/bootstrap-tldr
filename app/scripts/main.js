99262d4366304709fd49b004c9c607f54edeab1a
anvoz <anvo4888@gmail.com>
2014-03-12 19:57:45 +0700

    Prevent demo links from navigating Prevent demo forms from submitting

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,5 +4,14 @@
     $(function() {
         $('[data-toggle="tooltip"]').tooltip();
         $('[data-toggle="popover"]').popover();
+
+        // Prevent demo links from navigating
+        $('a[href="#"]:not([data-toggle], [rel="async"])').click(function() {
+            return false;
+        });
+        // Prevent demo forms from submitting
+        $('form:not([action])').submit(function() {
+            return false;
+        });
     });
 })(window);
