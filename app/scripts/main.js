77636693cc6299db9604fe0a9eb98457039a4d83
anvoz <anvo4888@gmail.com>
2014-03-20 00:11:01 +0700

    Basic HTML inspector button Added to typography section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,5 +13,28 @@
         $('form:not([action])').submit(function() {
             return false;
         });
+
+        // HTML inspector button
+        var inspectorParentCls = 'js-html-inspector',
+            inspectorBtnCls = 'js-btn-inspector',
+            $inspectorDialog = $('#dialog-html-inspector'),
+            $inspectorBtn = $('<button class="btn btn-default btn-xs ' + inspectorBtnCls + '">' +
+                '<span class="glyphicon glyphicon-search"></span>' +
+            '</button>');
+        $inspectorBtn.click(function() {
+            var $html = $(this).closest('.' + inspectorParentCls).clone();
+
+            // Clean some elements
+            $html.find('.' + inspectorBtnCls).remove();
+
+            // Copy html to dialog and launch
+            $inspectorDialog
+                .find('.modal-body pre').text($html.html()).end()
+                .modal();
+        });
+        // Add inspector button
+        $('.' + inspectorParentCls).hover(function() {
+            $(this).append($inspectorBtn);
+        });
     });
 })(window);
